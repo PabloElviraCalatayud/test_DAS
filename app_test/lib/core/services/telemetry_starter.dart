62c59027a1744c2a075c12bebe0b46b0602926d7
vicksbuup <victoria.v@alumnos.upm.es>
2026-01-30 12:39:51 +0100

    añadida comunicacion del bot de telegram

--- app_test/lib/core/services/telemetry_starter.dart
+++ app_test/lib/core/services/telemetry_starter.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../../data/sensors/imu/imu_sample.dart';
 import '../../data/sensors/imu/imu_store.dart';
@@ -7,7 +6,8 @@ import '../../data/sensors/pulse/pulse_sample.dart';
 import '../../data/sensors/pulse/pulse_store.dart';
 import '../../data/thingsboard/thingsboard_service.dart';
 import '../../data/thingsboard/thingsboard_service_config.dart';
-import 'service_locator.dart';
+import 'package:http/http.dart' as http;
+import '../telegram/telegram_message';
 import 'dart:math';
 
 ///funciones dummies, explicaciones de como deberia ser la funcion con sensores
@@ -28,24 +28,24 @@ class TelemetryStarter {
 
   //Se envian los datos al pulsar boton: sendPulseNow y sendImuNow
   Future<int?> sendPulseTelemetry() async  {
-    //PulseStore.instance.stream.listen((pulseState){
-    //cuando se tenga sensores
     final pulseState = PulseStore.instance.state;
     final pulse = pulseState.heartRate;
     if (pulse == null) return null;
 
+
     try {
       await tbService.sendTelemetry({"pulso": pulse});
       print("Pulso enviado: $pulse");
+      if(pulse>=100)
+        enviarAlerta();
+
     } catch (e) {
       print("Error al enviar pulso: $e");
     }
     return pulse;
-    //}); cierre de PulseStore.instance...
   }
 
   Future<ImuSample?> sendImuTelemetry() async {
-    //Simulacion de un sample
     final sample = ImuStore.instance.state.lastSample;
     if (sample == null) return null;
 
@@ -66,30 +66,4 @@ class TelemetryStarter {
     return sample;
   }
 
-//Realmente deberia quedar asi:
-/*
-  void startImuTelemetry() {
-
-    ImuStore.instance.stream.listen((imuState) {
-      final sample = imuState.lastSample;
-      if (sample == null) return;
-
-      tbService.sendTelemetry({
-        "acc_x": sample.axMs2,
-        "acc_y": sample.ayMs2,
-        "acc_z": sample.azMs2,
-        "gyro_x": sample.gxDps,
-        "gyro_y": sample.gyDps,
-        "gyro_z": sample.gzDps,
-      });
-    });
-      try {
-    await tbService.sendTelemetry(telemetry);
-    print("IMU enviada: $telemetry");
-  } catch (e) {
-    print("Error al enviar IMU: $e");
-  }
-  return sample;
-  }
-  */
 }
